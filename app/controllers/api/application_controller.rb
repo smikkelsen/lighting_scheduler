528b6a5a3943fb0c05201709b67c1dcfb9234664
Sean Mikkelsen <sean@mikkelsenfam.com>
2022-11-25 11:24:27 -0700

    fix api namespace

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,26 +1,22 @@
-module Api
-  module V1
-    class ApplicationController < ActionController::Base
-      before_action :authenticate_user_with_token
+class API::ApplicationController < ActionController::Base
+  before_action :authenticate_user_with_token
 
-      private
+  private
 
-      def authenticate_user_with_token
-        authenticate_or_request_with_http_basic do |api_key, api_token|
-          if ActiveSupport::SecurityUtils.secure_compare(api_key, ENV['API_KEY']) &&
-            ActiveSupport::SecurityUtils.secure_compare(api_token, ENV['API_TOKEN'])
-            return true
-          else
-            sleep 2
-            handle_bad_authentication
-          end
-        end
+  def authenticate_user_with_token
+    authenticate_or_request_with_http_basic do |api_key, api_token|
+      if ActiveSupport::SecurityUtils.secure_compare(api_key, ENV['API_KEY']) &&
+        ActiveSupport::SecurityUtils.secure_compare(api_token, ENV['API_TOKEN'])
+        return true
+      else
+        sleep 2
+        handle_bad_authentication
       end
-
-      def handle_bad_authentication
-        render json: { message: "Unauthorized" }, status: :unauthorized
-      end
-
     end
   end
+
+  def handle_bad_authentication
+    render json: { message: "Unauthorized" }, status: :unauthorized
+  end
+
 end
---
+++ app/controllers/api/v1/displays_controller.rb
@@ -0,0 +1,26 @@
+class API::V1::DisplaysController < API::ApplicationController
+  before_action :set_display, only: [:activate]
+
+  def index
+    @displays = Display.active
+    render json: @displays.to_json
+  end
+
+  def activate
+    @display.activate
+  end
+
+  def turn_off
+    Display.turn_off
+  end
+
+  private
+
+  def set_display
+    @display = Display.find(params[:id])
+  end
+
+  def display_params
+    params.require(:display).permit()
+  end
+end
